d3025bd37eeee66d1315b3e05247ae23ebc8eccb
Whaid Cremery <whaid.cremery@outlook.fr>
2020-02-08 00:47:18 +0100

    WCR - procedural generation

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-
 local dungeon = {}
 dungeon.width = 35
 dungeon.height = 15
@@ -25,7 +24,9 @@ function dungeonGenerator()
     local rooms = {}
     local nbRooms = 20
     local startLine, startColumn
-    local startRoom = {}
+    local startRoom = {}    
+    local currentRoom = {}    
+    local direction = ""
 
     print("Start dungeon generation...")
     for i=1, dungeon.nbLine, 1 do
@@ -36,14 +37,59 @@ function dungeonGenerator()
     end    
 
     -- where the dungeon start
-    startLine = math.random( 1, dungeon.nbLine)
-    startColumn = math.random(1, dungeon.nbColumn)
+    startLine = love.math.random(1, dungeon.nbLine)
+    startColumn = love.math.random(1, dungeon.nbColumn)
     startRoom = dungeon.map[startLine][startColumn]
     startRoom.isOpen = true
     startRoom.isStart = true
     -- add the first room in the list of rooms available
     table.insert( rooms, startRoom )
+    
+    -- in which direction it is possible to go
+    while #rooms < nbRooms do
+        local nextDirection = {}
+        local nextRoom = {}
+        currentRoomIndex = love.math.random(1, #rooms)
+        currentRoom = rooms[currentRoomIndex]
 
+        if currentRoom.line > 1 then
+            table.insert(nextDirection, "up")
+        end
+        if currentRoom.line < dungeon.nbLine then
+            table.insert(nextDirection, "down")
+        end
+        if currentRoom.column > 1 then
+            table.insert(nextDirection, "left")
+        end
+        if currentRoom.column < dungeon.nbColumn then
+            table.insert(nextDirection, "right")
+        end
+        
+        local genNumber = love.math.random(1, #nextDirection)
+        direction = nextDirection[genNumber]   
+        
+        if direction == "up" then
+            currentRoom.upDoor = true
+            nextRoom = dungeon.map[currentRoom.line-1][currentRoom.column]
+        elseif direction == "left" then
+            currentRoom.leftDoor = true
+            nextRoom = dungeon.map[currentRoom.line][currentRoom.column-1]
+        elseif direction == "down" then
+            currentRoom.bottomDoor = true
+            nextRoom = dungeon.map[currentRoom.line+1][currentRoom.column]
+        elseif direction == "right" then
+            currentRoom.rightDoor = true
+            nextRoom = dungeon.map[currentRoom.line][currentRoom.column+1]
+        else
+            print("FAIL !!!!!!")
+            return 1;
+        end
+
+        if nextRoom.isOpen == false then
+            nextRoom.isOpen = true 
+            table.insert(rooms, nextRoom)
+        end        
+    end
 end
 
 function love.load()
@@ -61,8 +107,8 @@ function love.draw()
             elseif dungeon.map[i][j].isStart == true then
                 love.graphics.setColor(0,1,0)
             end
-            love.graphics.rectangle("fill", dungeon.width * (j - 1) + (j * 5) , dungeon.height * (i - 1) + (i * 5), dungeon.width, dungeon.height)                    
+            love.graphics.rectangle("fill", dungeon.width * (j - 1) + (j * 5) , dungeon.height * (i - 1) + (i * 5), dungeon.width, dungeon.height)
+            love.graphics.setColor(1,1,1)                  
         end
-    end
-    love.graphics.setColor(255,255,255)
+    end    
 end
